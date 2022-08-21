96675425cef684b66211f25542d78e7edaec6cc5
yusufjon1997 <tomakeitworth@gmail.com>
2022-08-21 20:25:57 +0500

    wefwe

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
+import postcss from 'rollup-plugin-postcss';
 
 const packageJson = require('./package.json');
 
@@ -22,11 +23,13 @@ export default [
         plugins : [
             resolve(),
             commonjs(),
-            typescript({tsconfig : "./tsconfig.json"})
+            typescript({tsconfig : "./tsconfig.json"}),
+            postcss(),
         ]
     }, {
         input : "dist/esm/index.d.ts",
         output : [{ file : "dist/index.d.ts" , format : "esm"}],
         plugins : [dts()],
+        extarnal : [/\.css$/]
     }
 ]
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import './style.css';
-
+import './Button.css';
 
 interface ButtonProps {
     label : string
